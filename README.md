cf4da4395c8d2079d3cce1b05964cda7f397337b
Raí B. Toffoletto <rai@toffoletto.me>
2022-12-17 20:51:10 +0000

    feat: include versions 17 to 19

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # minecraft-server-install
-> Simple script to help install a vanilla minecraft server in debian/ubuntu systems (**Ubuntu 18.04 LTS** recommended).
->
-> To  operate the minecraftserver properly as an admin, be sure to know your minecraft UUID and your exact
-> USERNAME before the install. If you don't know it, then have a look at the usercache.json file after
-> login in and reinstall the server.
->
-> Versions available from 1.10 to 1.15.2
+
+Simple script to help install a vanilla minecraft server in debian/ubuntu systems (**Ubuntu 18.04 LTS** recommended).
+
+To operate the minecraftserver properly as an admin, be sure to know your minecraft UUID and your exact
+USERNAME before the install. If you don't know it, then have a look at the usercache.json file after
+login in and reinstall the server.
+
+> Versions available from 1.10 to 1.19.3
 
 ### Server initial dependencies:
-* git
-* python3
-* systemd
+
+- git
+- python3
+- systemd
 
 ### Cloning and Running:
+
 ```bash
 $ git clone https://github.com/raibtoffoletto/minecraft-server-install.git
 $ cd minecraft-server-install
 $ python3 minecraft-server-script.py
 ```
+
+## Acknowledgments
+
+Special thanks for the team over [MCVersions](https://mcversions.net/) to keep a comprehensive and complete list of Minecraft Servers.
+
+## Disclaimer
+
+This script is in no way associated with Mojang or Microsoft. You will need to purchase a legal copy of Minecraft client to enjoy this server.
--- minecraft-server-script.py
+++ minecraft-server-script.py
@@ -26,6 +26,12 @@
 import os, sys, time
 import shutil
 import subprocess
+import json
+
+servers = {}
+# Main list mantained by https://mcversions.net/
+with open('servers.json') as json_file:
+    servers: dict = json.load(json_file)
 
 def loading_cmd (message,waiting_cmd):
     while waiting_cmd.poll () is None:
@@ -69,64 +75,11 @@ subprocess.check_call (['sudo', 'apt-get', 'dist-upgrade', '-y'])
 subprocess.check_call (['sudo', 'apt-get', 'install', '-y', 'openjdk-8-jdk', 'wget'])
 
 print ("\n \n Preparations done. \n")
-server_version = ask_question (" Please, select the minecraft version to install (1.10 ~ 1.16.4): ",
-                ['1.16.4',
-                '1.16.3',
-                '1.16.2',
-                '1.16.1',
-                '1.16',
-                '1.15.2',
-                '1.15.1',
-                '1.15',
-                '1.14.4',
-                '1.14.3',
-                '1.14.2',
-                '1.14.1',
-                '1.14',
-                '1.13.2',
-                '1.13.1',
-                '1.13',
-                '1.12.2',
-                '1.12.1',
-                '1.12',
-                '1.11.2',
-                '1.11.1',
-                '1.11',
-                '1.10.2',
-                '1.10.1',
-                '1.10'], True)
+versions = list(servers.keys())
+server_version = ask_question (" Please, select the minecraft version to install (%s ~ %s): "%(versions[0], versions[-1]),
+                versions, True)
 
-# Main list mantained by https://mcversions.net/
-
-server_url = {
-    '1.16.4' : 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
-    '1.16.3' : 'https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar',
-    '1.16.2' : 'https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar',
-    '1.16.1' : 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
-    '1.16' : 'https://launcher.mojang.com/v1/objects/a0d03225615ba897619220e256a266cb33a44b6b/server.jar',
-    '1.15.2' : 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-    '1.15.1' : 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
-    '1.15' : 'https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar',
-    '1.14.4' : 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
-    '1.14.3' : 'https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar',
-    '1.14.2' : 'https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar',
-    '1.14.1' : 'https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar',
-    '1.14' : 'https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar',
-    '1.13.2' : 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
-    '1.13.1' : 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
-    '1.13' : 'https://launcher.mojang.com/v1/objects/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar',
-    '1.12.2' : 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
-    '1.12.1' : 'https://launcher.mojang.com/v1/objects/561c7b2d54bae80cc06b05d950633a9ac95da816/server.jar',
-    '1.12' : 'https://launcher.mojang.com/v1/objects/8494e844e911ea0d63878f64da9dcc21f53a3463/server.jar',
-    '1.11.2' : 'https://launcher.mojang.com/v1/objects/f00c294a1576e03fddcac777c3cf4c7d404c4ba4/server.jar',
-    '1.11.1' : 'https://launcher.mojang.com/v1/objects/1f97bd101e508d7b52b3d6a7879223b000b5eba0/server.jar',
-    '1.11' : 'https://launcher.mojang.com/v1/objects/48820c84cb1ed502cb5b2fe23b8153d5e4fa61c0/server.jar',
-    '1.10.2' : 'https://launcher.mojang.com/v1/objects/3d501b23df53c548254f5e3f66492d178a48db63/server.jar',
-    '1.10.1' : 'https://launcher.mojang.com/v1/objects/cb4c6f9f51a845b09a8861cdbe0eea3ff6996dee/server.jar',
-    '1.10' : 'https://launcher.mojang.com/v1/objects/a96617ffdf5dabbb718ab11a9a68e50545fc5bee/server.jar'
-}
-
-wget_server = subprocess.Popen (['wget','--no-check-certificate','-c', server_url.get (server_version)], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+wget_server = subprocess.Popen (['wget','--no-check-certificate','-c', servers.get (server_version)], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
 loading_cmd ('Getting minecraft-server.jar', wget_server)
 
 print ("\n \n Creating server folder and installing files. \n")
